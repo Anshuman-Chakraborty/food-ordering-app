3f4e03a0896733e526db7d5474eda1a82bf019c6
Anshuman Chakraborty <anshumanc.in@gmail.com>
2023-11-14 09:27:26 +0530

    redirecting from loginActivity to SignUpActivity

--- app/src/main/java/com/portfolio/friesandthighs/LoginActivity.kt
+++ app/src/main/java/com/portfolio/friesandthighs/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.portfolio.friesandthighs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.portfolio.friesandthighs.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding:ActivityLoginBinding by lazy{
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        binding.dontHaveTxt.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
